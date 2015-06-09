431d8e73b22e794a6101a483ae6559a378116eef
Brian Stern <briansternhk@gmail.com>
2015-06-09 16:32:58 -0400

    first commit

---
+++ server.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'httparty'
+require 'pry'
+require 'JSON'
+
+
+
+get "/" do
+  redirect "/catfacts"
+end
+
+get '/catfacts' do
+
+  response = HTTParty.get('http://catfacts-api.appspot.com/api/facts')
+  cat_response = JSON.parse(response)
+  cat_facts = cat_response["facts"][0]
+  erb :index, locals: { cat_facts: cat_facts }
+end
